dc05666b894fdbd3aa8ad0cbd6991b2378e7678b
Julz <13739471+julzpalomiano@users.noreply.github.com>
2024-08-27 13:10:46 +0800

    unlimited members

--- resources/views/livewire/team-and-title.blade.php
+++ resources/views/livewire/team-and-title.blade.php
@@ -189,11 +189,9 @@
                                 </div>
                                 <x-input-error :messages="$errors->get('members.' . $memberKey)" />
                             @endforeach
-                            @if (count($members) < 3)
-                                <button type="button" wire:click='addMember'
-                                    class="mt-2 px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">add
-                                    member</button>
-                            @endif
+                            <button type="button" wire:click='addMember'
+                                class="mt-2 px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">add
+                                member</button>
                         </div>
 
                         <div class="col-span-2">
@@ -334,11 +332,9 @@
                                 </div>
                                 <x-input-error :messages="$errors->get('members.' . $memberKey)" />
                             @endforeach
-                            @if (count($members) < 3)
-                                <button type="button" wire:click='addMember'
-                                    class="mt-2 px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">add
-                                    member</button>
-                            @endif
+                            <button type="button" wire:click='addMember'
+                                class="mt-2 px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">add
+                                member</button>
                         </div>
 
                         <div class="col-span-2">
