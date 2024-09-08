71d7fbfcaff3ce767a48354516e804da6bd299b3
Julz <13739471+julzpalomiano@users.noreply.github.com>
2024-09-08 15:11:18 +0800

    update schedule

--- resources/views/livewire/edit-team-and-title.blade.php
+++ resources/views/livewire/edit-team-and-title.blade.php
@@ -30,9 +30,9 @@
 
             <div class="col-span-2">
                 <label for="thesisTitle" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Thesis
-                    Title</label>
+                    Title {{ $team->schedule->type_of_defense ?? null }}</label>
 
-                @if ($team->schedule->type_of_defense != $team->schedule::TD)
+                @if (isset($team->schedule->type_of_defense) and $team->schedule->type_of_defense != $team->schedule::TD)
                     <input type="text" value="{{ $thesisTitle }}" disabled
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                 @else
